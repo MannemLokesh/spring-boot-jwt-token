d24e62c00be4657d42123f2effcdc63db35b6869
Mannem Lokesh <mannemlokesh@EXAFLUENCE.LOCAL>
2023-06-07 20:05:47 +0530

    initial commit

---
+++ spring-boot3-jwt-main/src/main/java/com/javatechie/controller/ProductController.java
@@ -0,0 +1,75 @@
+package com.javatechie.controller;
+
+import com.javatechie.dto.AuthRequest;
+import com.javatechie.dto.Product;
+import com.javatechie.entity.UserInfo;
+import com.javatechie.service.JwtService;
+import com.javatechie.service.ProductService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public List<Product> getAllTheProducts(HttpServletRequest request) {
+    	System.out.println(jwtService.extractUsername(request.getHeader("Authorization").substring(7)));//decode username from token
+//    	jwtService.
+    	System.out.println("controll in all");
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('USER','ADMIN')")
+    public Product getProductById(@PathVariable int id) {
+    	System.out.println("controll in id");
+        return service.getProduct(id);
+    }
+
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+    	System.out.println(authRequest.getUsername()+"    "+authRequest.getPassword());
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        
+        System.out.println(authRequest.getUsername()+"    "+authRequest.getPassword());
+        
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+
+
+    }
+}
